74aac7eb04022da9931b6ed8dbaa6c3f2f935cad
evellyinhelenoir <evellyin.nunes@zup.com.br>
2021-11-12 11:41:57 -0300

    Criar Método 'exibirTodasCotas' em ContaService

--- src/main/java/br/com/zup/DesafioModulo05Conta/conta/ContaService.java
+++ src/main/java/br/com/zup/DesafioModulo05Conta/conta/ContaService.java
@@ -3,6 +3,8 @@ package br.com.zup.DesafioModulo05Conta.conta;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ContaService {
     @Autowired
@@ -11,5 +13,9 @@ public class ContaService {
     public Conta salvarConta (Conta conta){
 
         return contaRepository.save(conta);
+    }
+    public List<Conta> exibirTodasContas(){
+        List<Conta> contas = (List<Conta>) contaRepository.findAll();
+        return contas;
     }
 }
