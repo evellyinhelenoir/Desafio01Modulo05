871228c0982f20b61c7c2508d55ac157ea6247b5
evellyinhelenoir <evellyin.nunes@zup.com.br>
2021-11-12 11:48:07 -0300

    Criar Classe ContaController

---
+++ src/main/java/br/com/zup/DesafioModulo05Conta/conta/ContaController.java
@@ -0,0 +1,9 @@
+package br.com.zup.DesafioModulo05Conta.conta;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/conta")
+public class ContaController {
+}
