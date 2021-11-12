f7119f0fdb29148fcc532a1eb5c78bd8de115f61
evellyinhelenoir <evellyin.nunes@zup.com.br>
2021-11-12 11:59:21 -0300

    Criar Método 'realizarCadastroConta' em ContaController

--- src/main/java/br/com/zup/DesafioModulo05Conta/conta/ContaController.java
+++ src/main/java/br/com/zup/DesafioModulo05Conta/conta/ContaController.java
@@ -1,9 +1,13 @@
 package br.com.zup.DesafioModulo05Conta.conta;
 
+import br.com.zup.DesafioModulo05Conta.dtos.ResumoEntradaDTO;
+import br.com.zup.DesafioModulo05Conta.dtos.ResumoSaidaDTO;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/conta")
@@ -13,4 +17,11 @@ public class ContaController {
     private ContaService contaService;
     @Autowired
     private ModelMapper modelMapper;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResumoSaidaDTO realizarCadastroConta(@RequestBody @Valid ResumoEntradaDTO resumoEntradaDTO){
+        Conta conta = modelMapper.map(resumoEntradaDTO, Conta.class);
+        return modelMapper.map(contaService.salvarConta(conta), ResumoSaidaDTO.class);
+    }
 }
