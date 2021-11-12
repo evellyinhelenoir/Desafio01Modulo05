70bcca3d3674cc5ee692f98d5134d0941a83a1fa
evellyinhelenoir <evellyin.nunes@zup.com.br>
2021-11-12 12:00:51 -0300

    Criar Método 'exibirTodasContas' em ContaController

--- src/main/java/br/com/zup/DesafioModulo05Conta/conta/ContaController.java
+++ src/main/java/br/com/zup/DesafioModulo05Conta/conta/ContaController.java
@@ -1,5 +1,6 @@
 package br.com.zup.DesafioModulo05Conta.conta;
 
+import br.com.zup.DesafioModulo05Conta.dtos.ResumoContaDTO;
 import br.com.zup.DesafioModulo05Conta.dtos.ResumoEntradaDTO;
 import br.com.zup.DesafioModulo05Conta.dtos.ResumoSaidaDTO;
 import org.modelmapper.ModelMapper;
@@ -8,6 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/conta")
@@ -23,5 +26,14 @@ public class ContaController {
     public ResumoSaidaDTO realizarCadastroConta(@RequestBody @Valid ResumoEntradaDTO resumoEntradaDTO){
         Conta conta = modelMapper.map(resumoEntradaDTO, Conta.class);
         return modelMapper.map(contaService.salvarConta(conta), ResumoSaidaDTO.class);
+    }
+    @GetMapping
+    public List<ResumoContaDTO> exibirTodasContas(){
+        List<ResumoContaDTO> contasDTO = new ArrayList<>();
+        for (Conta contas : contaService.exibirTodasContas()){
+            ResumoContaDTO resumoContaDTO = modelMapper.map(contas, ResumoContaDTO.class);
+            contasDTO.add(resumoContaDTO);
+        }
+        return contasDTO;
     }
 }
