95593966a1f8e3631b0573c9dac8365ee96dbe6e
evellyinhelenoir <evellyin.nunes@zup.com.br>
2021-11-12 11:22:52 -0300

    Criar pacotes

---
+++ src/main/java/br/com/zup/DesafioModulo05Conta/DesafioModulo05ContaApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.DesafioModulo05Conta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioModulo05ContaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioModulo05ContaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/DesafioModulo05Conta/DesafioModulo05ContaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.DesafioModulo05Conta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioModulo05ContaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
