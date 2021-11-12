3a0f9532e7bbc86520e89ecd72123f9be9b56c41
evellyinhelenoir <evellyin.nunes@zup.com.br>
2021-11-12 11:51:13 -0300

    Linkando ContaController com ContaService

--- src/main/java/br/com/zup/DesafioModulo05Conta/conta/ContaController.java
+++ src/main/java/br/com/zup/DesafioModulo05Conta/conta/ContaController.java
@@ -1,9 +1,14 @@
 package br.com.zup.DesafioModulo05Conta.conta;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/conta")
 public class ContaController {
+
+    @Autowired
+    private ContaService contaService;
+
 }
