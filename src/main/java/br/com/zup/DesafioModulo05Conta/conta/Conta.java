6c75899ad6e53591ab1688e380ceac0a2cbce128
evellyinhelenoir <evellyin.nunes@zup.com.br>
2021-11-12 11:26:30 -0300

    Criar Model Conta

---
+++ src/main/java/br/com/zup/DesafioModulo05Conta/conta/Conta.java
@@ -0,0 +1,94 @@
+package br.com.zup.DesafioModulo05Conta.conta;
+
+import br.com.zup.DesafioModulo05Conta.enums.Status;
+import br.com.zup.DesafioModulo05Conta.enums.Tipo;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "contas")
+public class Conta {
+    private String nome;
+    private double valor;
+    @Column(name = "data_de_vencimento")
+    private LocalDate dataVencimento;
+    @Column(name = "data_de_pagamento")
+    private LocalDateTime dataPagamento;
+    @Enumerated(EnumType.STRING)
+    private Status status;
+    @Enumerated(EnumType.STRING)
+    private Tipo tipo;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    public Conta() {
+    }
+
+    public Conta(String nome, double valor, LocalDate dataVencimento, LocalDateTime dataPagamento, Status status, Tipo tipo, int id) {
+        this.nome = nome;
+        this.valor = valor;
+        this.dataVencimento = dataVencimento;
+        this.dataPagamento = dataPagamento;
+        this.status = status;
+        this.tipo = tipo;
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public LocalDate getDataVencimento() {
+        return dataVencimento;
+    }
+
+    public void setDataVencimento(LocalDate dataVencimento) {
+        this.dataVencimento = dataVencimento;
+    }
+
+    public LocalDateTime getDataPagamento() {
+        return dataPagamento;
+    }
+
+    public void setDataPagamento(LocalDateTime dataPagamento) {
+        this.dataPagamento = dataPagamento;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
