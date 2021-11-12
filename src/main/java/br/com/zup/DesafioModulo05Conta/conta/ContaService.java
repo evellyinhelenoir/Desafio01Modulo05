ecb876f43223330fd6d333da6f33bacaf1173b0c
evellyinhelenoir <evellyin.nunes@zup.com.br>
2021-11-12 11:40:20 -0300

    Linkando Service com Repository

--- src/main/java/br/com/zup/DesafioModulo05Conta/conta/ContaService.java
+++ src/main/java/br/com/zup/DesafioModulo05Conta/conta/ContaService.java
@@ -1,7 +1,10 @@
 package br.com.zup.DesafioModulo05Conta.conta;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ContaService {
+    @Autowired
+    private ContaRepository contaRepository;
 }
