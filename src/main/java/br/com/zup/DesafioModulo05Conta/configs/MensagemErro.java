5bcb52a3d1dbbea0f3d6f08490475a18d6d15cfc
evellyinhelenoir <evellyin.nunes@zup.com.br>
2021-11-12 12:15:50 -0300

    Criar Classe MensagemErro

---
+++ src/main/java/br/com/zup/DesafioModulo05Conta/configs/MensagemErro.java
@@ -0,0 +1,17 @@
+package br.com.zup.DesafioModulo05Conta.configs;
+
+public class MensagemErro {
+    private String mensagemErro;
+
+    public MensagemErro(String mensagemErro) {
+        this.mensagemErro = mensagemErro;
+    }
+
+    public String getMensagemErro() {
+        return mensagemErro;
+    }
+
+    public void setMensagemErro(String mensagemErro) {
+        this.mensagemErro = mensagemErro;
+    }
+}
